f74356d6614b36fd79e8d5b35460c7c6d95d112b
xchen205 <xchen205@hawk.iit.edu>
2024-11-20 09:23:01 -0600

    move function back to init, undo var

--- js/script.js
+++ js/script.js
@@ -1,32 +1,28 @@
 function init(){
   if(window.location.pathname == "/webdev_project3/apiuserpage.html"){
-    loadAPIs();
+    //cat variables
+    const catButton = document.getElementById("cat-button");
+    const catClearButton = document.getElementById("cat-clear-button");
+    catButton.disabled = true;
+    catClearButton.disabled = true;
+    document.getElementById("less-button").disabled = true;
+    //color buttons
+    const colorButton = document.getElementById("color-button");
+    const paletteButton = document.getElementById("palette-button");
+    const colorClearButton = document.getElementById("color-clear-button");
+    //cat buttons
+    //catButton.addEventListener("onclick", getDogFact);
+    //catClearButton.addEventListener("onclick", removeCatFact);
+    //color buttons
+    colorButton.addEventListener("onclick", getColourLoversColor);
+    paletteButton.addEventListener("onclick", getColourLoversPalette);
+    colorClearButton.addEventListener("onclick", removeColourLoversPalette());
+    getColourLoversPalette();
+    //getDogFact();
   }
 }
 
 window.addEventListener("load", init);
-
-function loadAPIs(){
-  //cat variables
-  var catButton = document.getElementById("cat-button");
-  var catClearButton = document.getElementById("cat-clear-button");
-  catButton.disabled = true;
-  catClearButton.disabled = true;
-  document.getElementById("less-button").disabled = true;
-  //color buttons
-  var colorButton = document.getElementById("color-button");
-  var paletteButton = document.getElementById("palette-button");
-  var colorClearButton = document.getElementById("color-clear-button");
-  //cat buttons
-  //catButton.addEventListener("onclick", getDogFact);
-  //catClearButton.addEventListener("onclick", removeCatFact);
-  //color buttons
-  colorButton.addEventListener("onclick", getColourLoversColor);
-  paletteButton.addEventListener("onclick", getColourLoversPalette);
-  colorClearButton.addEventListener("onclick", removeColourLoversPalette());
-  getColourLoversPalette();
-  //getDogFact();
-}
 
 function getColourLoversColor(){
   //uses ColourLovers API
