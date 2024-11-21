388589d93216cacc69a7f2d3475d9889fd75a100
xchen205 <xchen205@hawk.iit.edu>
2024-11-21 00:55:25 -0600

    map code

--- js/script.js
+++ js/script.js
@@ -158,3 +158,12 @@ function removeColourLoversPalette(){
   document.getElementById("color-pic").src = "";
 }
 
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
